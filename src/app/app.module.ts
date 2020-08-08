b7d2636b07fc87332f15ffeb570e7bc2f248054d
Akash Jaiswal <akashjaiswal@iiitdmj.ac.in>
2020-08-08 14:25:05 +0530

    Added frame component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PreloaderComponent } from './preloader/preloader.component';
+import { FrameComponent } from './frame/frame.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PreloaderComponent
+    PreloaderComponent,
+    FrameComponent
   ],
   imports: [
     BrowserModule,
